c30e4d962cf3dfa5e2beee96e866189c203d0475
VitorFranciscoDev <vitorfndr@gmail.com>
2025-12-20 13:35:15 -0300

    feature(): Auth Repository done

---
+++ src/domain/repositories/auth_repository.ts
@@ -0,0 +1,8 @@
+import { User } from "../entities/user";
+
+export interface AuthRepository {
+    getUserByID(id: number): Promise<User>
+    getUserByUUID(uuid: string): Promise<User>
+    addUser(user: User): Promise<void>
+    deleteUser(user: User): Promise<void>
+}
